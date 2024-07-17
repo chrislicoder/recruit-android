00dbbe918eae1b8df326a50e2a135ea7d3d75b2d
Chris Li <chrislicoder@gmail.com>
2024-07-17 19:38:00 +1200

    Consumes the fetched transaction list and display transaction list result on the

--- app/src/main/java/nz/co/test/transactions/fragments/TransactionListFragment.kt
+++ app/src/main/java/nz/co/test/transactions/fragments/TransactionListFragment.kt
@@ -4,27 +4,46 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import nz.co.test.transactions.R
 import nz.co.test.transactions.uicomponents.TransactionAdapter
 import nz.co.test.transactions.viewmodel.TransactionListViewModel
 
 @AndroidEntryPoint
 class TransactionListFragment : Fragment(R.layout.fragment_transaction_list) {
-
     private val viewModel: TransactionListViewModel by viewModels()
+    private lateinit var transactionList: RecyclerView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView(view)
+    }
 
-        // Assuming you have a RecyclerView with ID `recyclerView`
-        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+    private fun setupRecyclerView(view: View) {
+        transactionList = view.findViewById(R.id.recyclerView)
+
         val adapter = TransactionAdapter { transactionId ->
             findNavController().navigate(R.id.action_transactionListFragment_to_transactionDetailFragment)
         }
-        recyclerView.adapter = adapter
+        transactionList.layoutManager = LinearLayoutManager(context)
+        transactionList.adapter = adapter
 
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.transactions.collectLatest { transactions ->
+                    if (transactions.isEmpty()) return@collectLatest
+                    adapter.updateTransaction(transactions.toList())
+                }
+            }
+        }
         viewModel.fetchTransactions()
     }
 }
--- app/src/main/java/nz/co/test/transactions/uicomponents/TransactionAdapter.kt
+++ app/src/main/java/nz/co/test/transactions/uicomponents/TransactionAdapter.kt
@@ -3,14 +3,22 @@ package nz.co.test.transactions.uicomponents
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import nz.co.test.transactions.R
 import nz.co.test.transactions.services.Transaction
+import java.text.DecimalFormat
+import java.time.format.DateTimeFormatter
 
 class TransactionAdapter(private val clickListener: (Int) -> Unit) :
     RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
 
-    private val transactions = emptyList<Transaction>()
+    private var transactions = emptyList<Transaction>()
+
+    fun updateTransaction(transactions: List<Transaction>) {
+        this.transactions = transactions
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
         val view =
@@ -25,7 +33,17 @@ class TransactionAdapter(private val clickListener: (Int) -> Unit) :
     override fun getItemCount() = transactions.size
 
     class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val transactionDateTextView: TextView = itemView.findViewById(R.id.transactionDateTextView)
+        private val summaryTextView: TextView = itemView.findViewById(R.id.summaryTextView)
+        private val debitTextView: TextView = itemView.findViewById(R.id.debitTextView)
+        private val creditTextView: TextView = itemView.findViewById(R.id.creditTextView)
+
         fun bind(transaction: Transaction, clickListener: (Int) -> Unit) {
+            transactionDateTextView.text = transaction.transactionDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))
+            summaryTextView.text = transaction.summary
+            debitTextView.text = DecimalFormat.getCurrencyInstance().format(transaction.debit)
+            creditTextView.text = DecimalFormat.getCurrencyInstance().format(transaction.credit)
+
             // Bind transaction data to views
             itemView.setOnClickListener {
                 clickListener(transaction.id)
